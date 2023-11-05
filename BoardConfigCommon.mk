4f606adce2a2d9ef2f923fc9cd6afcfec93155c9
Sonal Singh <sonal.singh.19993@gmail.com>
2023-11-06 00:30:07 +0530

    sm6250-common: Update sepolicy inclusion path
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -184,7 +184,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := 2023-07-01
 
 # Sepolicy
-include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
+include device/qcom/sepolicy_vndr/SEPolicy.mk
 
 SELINUX_IGNORE_NEVERALLOWS := true
 
