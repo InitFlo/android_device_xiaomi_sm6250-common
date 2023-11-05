6e69f4c66b6f5e731b25ddac4fcfea1128a9f831
Sonal Singh <sonal.singh.19993@gmail.com>
2023-11-05 23:41:27 +0530

    sm6250-common: Drop Lineage health and fastcharge implementation
    Revert "sm6250-common: overlay: Enable Lineage fastcharge preference"
    
    This reverts commit 801aced795ca856273c5f7da86715782b973764d.
    
    Revert "sm6250-common: fastcharge: Extend Fastcharge implementation"
    
    This reverts commit 4ac16d9c0f70c60ebb11c0ec734e80e80fdbbed3.
    
    Revert "[SQUASH] sm6250-common: Fastcharge implementation"
    
    This reverts commit 7f8cfd0976feda9640c9b699dee6c6c3e7a9a5f6.
    
    Revert "sm6250-common: overlay: Enable Lineage Health service"
    
    This reverts commit b7ea872e539abf0813737ba2808aadf750f2bec5.
    
    Revert "sm6250-common: Build Lineage Health HAL"
    
    This reverts commit 287b9d0f05e6a3a0e1da88c2905a6388db561da2.

--- common.mk
+++ common.mk
@@ -170,10 +170,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
-# Fastcharge
-PRODUCT_PACKAGES += \
-    vendor.lineage.fastcharge@1.0-service.xiaomi_sm6250
-
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.xiaomi
@@ -289,10 +285,6 @@ PRODUCT_PACKAGES += \
 # Light
 PRODUCT_PACKAGES += \
     android.hardware.light-service.xiaomi
-
-# Lineage Health
-PRODUCT_PACKAGES += \
-    vendor.lineage.health-service.default
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
